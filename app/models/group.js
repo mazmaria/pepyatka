412332b5b5daec85add8172a256d32f4de2115cd
mazmaria <mazmariya@gmail.com>
2013-05-30 18:51:51 +0300

    Fixes admins listing, removing last admin, adding new admin

--- app/models/group.js
+++ app/models/group.js
@@ -297,11 +297,13 @@ exports.addModel = function(db) {
 
   Group.prototype.addAdministrator = function(feedId, callback) {
     var that = this
-    if (feedId !== undefined) {
-      db.zadd('user:' + that.id + ':administrators', new Date().getTime().toString(), feedId, function(err, res) {
-        callback(err, res)
-      })
-    }
+
+    if (feedId !== undefined) return callback(1, null)
+
+    db.zadd('user:' + that.id + ':administrators', new Date().getTime().toString(), feedId, function(err, res) {
+      callback(err, res)
+    })
+    
   }
 
   Group.prototype.removeAdministrator = function(feedId, callback) {
@@ -315,6 +317,14 @@ exports.addModel = function(db) {
         return callback(null)
 
       if (administratorsIds.length === 1)
+        return callback(1)
+
+      var adminsNumber = administratorsIds.length
+      async.map(administratorsIds, function(administratorId, callback) {
+        if (administratorId === 'undefined') adminsNumber--
+      })
+
+      if (adminsNumber === 1)
         return callback(1)
 
       db.zrem('user:' + that.id + ':administrators', feedId, function(err, res) {
@@ -364,6 +374,7 @@ exports.addModel = function(db) {
     if (select.indexOf('admins') != -1) {
       that.getAdministratorsIds(function(err, administratorsIds) {
         async.map(administratorsIds, function(administratorId, callback) {
+          if (administratorId === 'undefined') return callback(err, null)
           models.FeedFactory.findById(administratorId, function(err, user) {
             user.toJSON({ select: ['id', 'username'] }, function(err, json) {
               callback(err, json)
